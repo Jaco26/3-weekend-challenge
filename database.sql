288dcf74d94da4286a544ba7a12185f67944fa52
Jacob Albright <jacob.albright23@gmail.com>
2018-02-17 14:25:24 -0600

    updated the database.sql file to reflect my current table structure

--- database.sql
+++ database.sql
@@ -1,8 +1,20 @@
-CREATE TABLE tasks (
+CREATE TABLE tasks
+(
 	id SERIAL PRIMARY KEY,
 	task VARCHAR(120),
 	category VARCHAR(40),
+	notes VARCHAR(240),
 	completed VARCHAR(2),
 	due_date DATE,
 	date_added DATE
+);
+
+CREATE TABLE completed_tasks
+(
+	id SERIAL PRIMARY KEY,
+	task VARCHAR(120),
+	category VARCHAR(40),
+	notes VARCHAR(240),
+	due_date DATE,
+	date_completed DATE
 );
