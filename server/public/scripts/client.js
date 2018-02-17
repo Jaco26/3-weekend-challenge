8f6741f27ce7430a390e16c39d7e22ef91218058
Jacob Albright <jacob.albright23@gmail.com>
2018-02-16 18:28:10 -0600

    task-input fields have pretty good form validation on them now

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-    
-});
+    $('#submitTask').on('click', packageNewTask);
+}); // END document.ready
+
+function packageNewTask(){
+    let newTask = {task: $('#taskIn').val(), dueDate: $('#dueDateIn').val(), completed: $('#completedYN').val()};
+    let isValid = 0;
+    let keys = Object.keys(newTask);
+    for(let i = 0; i < keys.length; i ++){
+        if(newTask[keys[i]].length === 0){
+           isValid += 1
+        }
+    }
+    if(isValid > 0){
+        alert('NO')
+    } else {
+        console.log(newTask);
+        // sendTask(newTask);
+        clearTasksInFields()   
+    }
+} // END #submitTask onclick
+
+function sendTask(taskObj){
+    $.ajax()
+} // END sendTask
+
+function clearTasksInFields(){
+    $('#taskIn').val('');
+    $('#dueDateIn').val('');
+    $('#completedYN').val('no')
+}
